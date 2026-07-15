034ab5efe3103e4149b731719488cafd6060a060
Josh <joshnichols84@me.com>
2026-07-15 15:02:15 -0400

    created .bat files to launch directly to browser

---
+++ main.bat
@@ -0,0 +1,3 @@
+start E:\Coding\video_game_library\second.bat
+timeout /T 2
+start http://127.0.0.1:5000
---
+++ second.bat
@@ -0,0 +1,4 @@
+cd /D E:\Coding\video_game_library
+call venv\Scripts\activate
+flask run
+pause
